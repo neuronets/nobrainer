dd5553beadefafe873b91c6ad44825aba0a03b46
= <jakubk@mit.edu>
2018-01-30 19:32:35 -0500

    initial tensorflow implementation of highres3dnet

--- nobrainer/models/highres3dnet.py
+++ nobrainer/models/highres3dnet.py
@@ -1,114 +1,126 @@
-"""HighRes3DNet for Keras.
+"""HighRes3DNet implemented in TensorFlow.
 
-Example
--------
->>> volume_shape = (256, 256, 256, 1)
->>> model = HighRes3DNet(n_classes=60, input_shape=volume_shape)
->>> model.compile(optimizer='adam', loss=dice_loss, metrics=[dice_coef])
+Notes
+-----
+This script was written with the help of
+https://github.com/tensorflow/models/blob/178480ed3be30bc33dafead53b5ee09c717fa2b7/official/resnet/resnet_model.py.
 
 Reference
 ---------
-[On the Compactness, Efficiency, and Representation of 3D Convolutional Networks: Brain Parcellation as a Pretext Task](https://doi.org/10.1007/978-3-319-59050-9_28)
+Li W., Wang G., Fidon L., Ourselin S., Cardoso M.J., Vercauteren T. (2017)
+On the Compactness, Efficiency, and Representation of 3D Convolutional
+Networks: Brain Parcellation as a Pretext Task. In: Niethammer M. et al. (eds)
+Information Processing in Medical Imaging. IPMI 2017. Lecture Notes in Computer
+Science, vol 10265.
 """
 
-from tensorflow.python.keras import backend as K
-from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.layers import (
-    Activation, Add, BatchNormalization, Conv3D, Input
-)
+import tensorflow as tf
+
+_BATCH_NORM_DECAY = 0.997
+_BATCH_NORM_EPSILON = 1e-5
+
+tf.logging.set_verbosity(tf.logging.INFO)
 
 
-# https://github.com/jocicmarko/ultrasound-nerve-segmentation/blob/27c47a8f38f11e446e33465146c8eb6074872678/train.py#L23-L27
-def dice_coef(y_true, y_pred, smooth=1.):
-    """Return Dice coefficient given two boolean ndarrays."""
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+def _batch_norm_relu(inputs, is_training, data_format):
+    """Perform batch normalization and ReLU on `inputs`."""
+    inputs = tf.layers.batch_normalization(
+        inputs=inputs, axis=1 if data_format == 'channels_first' else 3,
+        momentum=_BATCH_NORM_DECAY, epsilon=_BATCH_NORM_EPSILON, center=True,
+        scale=True, training=is_training, fused=True,
+    )
+    return tf.nn.relu(inputs)
 
 
-def dice_loss(y_true, y_pred, smooth=1):
-    """Return Dice loss given two boolean ndarrays."""
-    return 1 - dice_coef(y_true, y_pred, smooth)
+def _building_block(inputs, filters, dilation_rate, is_training, strides,
+                    data_format):
+    """Perform two rounds of batch normalization, ReLU, and 3D convolution,
+    and sum original inputs with ouputs."""
+    shortcut = inputs
+
+    for _ in range(2):
+        inputs = _batch_norm_relu(inputs, is_training, data_format)
+        inputs = tf.layers.conv3d(
+            inputs=inputs, filters=filters, kernel_size=(3, 3, 3),
+            strides=strides, padding='same', data_format=data_format,
+            dilation_rate=dilation_rate,
+        )
+
+    return inputs + shortcut
 
 
-def HighRes3DNet(n_classes, input_tensor=None, input_shape=None):
-    """Instantiates the HighRes3DNet architecture.
+def _block_layer(inputs, filters, dilation_rate, num_blocks, strides,
+                 is_training, name, data_format):
+    """Return blocks of blocks buildings."""
+    for _ in range(1, num_blocks):
+        inputs = _building_block(
+            inputs=inputs, filters=filters, dilation_rate=dilation_rate,
+            is_training=is_training, strides=strides, data_format=data_format,
+        )
+    return tf.identity(inputs, name=name)
+
+
+def highres3dnet(features, labels, num_classes, is_training, data_format=None):
+    """Return a HighRes3DNet model.
+
     Parameters
     ----------
-    n_classes : int
+    num_classes : int
         Number of classes to output in the last convolutional layer.
-    input_tensor : optional Keras tensor (i.e., output of `layers.Input()`) to
-        use as image input for the model. Does not have to be provided if
-        `input_shape` is given.
+    input_tensor : optional tensor to use as image input for the model. Does
+        not have to be provided if `input_shape` is given.
     input_shape : optional shape tuple. Does not have to be provided if
         `input_tensor` is given.
+
     Returns
     -------
-    A Keras model instance.
+    A TensorFlow model.
     """
-    if input_tensor is not None:
-        inputs = input_tensor
-    elif input_shape is not None:
-        inputs = Input(shape=input_shape, name='input')
-    else:
-        raise ValueError("`input_tensor` or `input_shape` must be provided.")
+    if data_format is None:
+        # 'channels_first' is typically faster on GPU. 'channels_last' is
+        # typically faster on CPU.
+        data_format = (
+            'channels_first' if tf.test.is_built_with_cuda()
+            else 'channels_last'
+        )
 
-    # Block 0
-    x = Conv3D(16, (3, 3, 3), padding='same', name='conv_0')(inputs)
-    x = BatchNormalization(name='norm_0')(x)
-    x = Activation('relu', name='activ_0')(x)
+    # Initial block.
+    inputs = tf.layers.conv3d(
+        inputs=features, filters=16, kernel_size=(3, 3, 3), padding='same',
+        name='initial_conv',
+    )
+    inputs = _batch_norm_relu(
+        inputs, is_training=is_training, data_format=data_format
+    )
+    inputs = tf.identity(inputs, name='initial_block')
 
-    names = {
-        'activ': 'activ_{}_{}',
-        'conv': 'conv_{}_{}',
-        'norm': 'norm_{}_{}',
-        'add': 'add_{}',
-    }
+    # 3 residual blocks.
+    inputs = _block_layer(
+        inputs=inputs, filters=16, dilation_rate=(1, 1, 1), num_blocks=3,
+        strides=1, is_training=is_training, name='block_layer1',
+        data_format=data_format,
+    )
 
-    # Blocks 1-3 (each block has 2 conv3d layers).
-    _offset = 1
-    for ii in range(3):
-        residual = x
-        for jj in range(2):
-            # suffix = "_{}".format(2 * ii + jj + _offset)
-            suffix = "_{}_{}".format(ii + _offset, jj)
-            x = BatchNormalization(name='norm' + suffix)(x)
-            x = Activation('relu', name='activ' + suffix)(x)
-            x = Conv3D(16, (3, 3, 3), padding='same', name='conv' + suffix)(x)
-        x = Add(name=names['add'].format(ii + _offset))([residual, x])
+    # 3 residual blocks.
+    inputs = _block_layer(
+        inputs=inputs, filters=32, dilation_rate=(2, 2, 2), num_blocks=3,
+        strides=1, is_training=is_training, name='block_layer2',
+        data_format=data_format,
+    )
 
-    # Blocks 4-6 (each block has 2 conv3d layers).
-    for ii in range(3):
-        residual = x
-        _offset = 4
-        for jj in range(2):
-            suffix = "_{}_{}".format(ii + _offset, jj)
-            x = BatchNormalization(name='norm' + suffix)(x)
-            x = Activation('relu', name='activ' + suffix)(x)
-            # TODO: confirm that filters=16 and dilation_rate=(2, 2, 2) gives a
-            # convolution with 32 kernels dilated by 2.
-            x = Conv3D(16, (3, 3, 3), padding='same', dilation_rate=(2, 2, 2),
-                       name='conv' + suffix)(x)
-        x = Add(name=names['add'].format(ii + _offset))([residual, x])
+    # 3 residual blocks.
+    inputs = _block_layer(
+        inputs=inputs, filters=64, dilation_rate=(4, 4, 4), num_blocks=3,
+        strides=1, is_training=is_training, name='block_layer3',
+        data_format=data_format,
+    )
 
-    # Blocks 7-9 (each block has 2 conv3d layers).
-    for ii in range(3):
-        residual = x
-        _offset = 7
-        for jj in range(2):
-            suffix = "_{}_{}".format(ii + _offset, jj)
-            x = BatchNormalization(name='norm' + suffix)(x)
-            x = Activation('relu', name='activ' + suffix)(x)
-            # TODO: confirm that filters=16 and dilation_rate=(4, 4, 4) gives a
-            # convolution with 64 kernels dilated by 4.
-            x = Conv3D(16, (3, 3, 3), padding='same', dilation_rate=(4, 4, 4),
-                       name='conv' + suffix)(x)
-        x = Add(name=names['add'].format(ii + _offset))([residual, x])
+    # Final classification layer.
+    inputs = tf.layers.conv3d(
+        inputs=inputs, filters=num_classes, kernel_size=(1, 1, 1),
+        padding='same',
+    )
+    probabilities = tf.nn.softmax(logits=inputs)
+    probabilities = tf.identity(inputs, name='classification')
 
-    # Block 19
-    x = Conv3D(n_classes, (1, 1, 1), name='classification')(x)
-    x = Activation('softmax', name='final_activation')(x)
-
-    return Model(inputs, x, name='highres3dnet')
-
+    return probabilities
