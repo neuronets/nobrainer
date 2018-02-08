3c0f5bb4d1b32a1fe226f90a922289125a35699d
= <jakubk@mit.edu>
2018-02-08 09:14:46 -0500

    use tensorflow math ops

--- nobrainer/losses.py
+++ nobrainer/losses.py
@@ -2,14 +2,14 @@
 
 import tensorflow as tf
 
-from nobrainer import scores
+from nobrainer import metrics
 
 
 def dice_loss(labels, predictions):
     """Return Dice loss given two tensors. Output is in range [0, 1]."""
     with tf.name_scope('dice_loss'):
-        best = tf.constant(1., dtype=tf.float64)
-        return tf.subtract(best, scores.dice_coefficient(labels, predictions))
+        best = tf.constant(1., dtype=tf.float32)
+        return tf.subtract(best, metrics.dice_coefficient(labels, predictions))
 
 
 def hamming_loss(labels, predictions):
@@ -17,5 +17,5 @@ def hamming_loss(labels, predictions):
     # QUESTION: does this implementation make sense?
     with tf.name_scope('hamming_loss'):
         return tf.truediv(
-            scores.hamming_distance(labels, predictions), tf.size(labels)
+            metrics.hamming_distance(labels, predictions), tf.size(labels)
         )
