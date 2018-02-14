b2cdb06d3c7047e77d344f1a0d5d067fa292da87
= <jakubk@mit.edu>
2018-02-14 11:35:07 -0500

    add

--- nobrainer/util.py
+++ nobrainer/util.py
@@ -1,5 +1,7 @@
 """Utilities."""
 
+import numpy as np
+
 
 def _shapes_equal(x1, x2):
     """Return whether shapes of arrays or tensors `x1` and `x2` are equal."""
@@ -16,3 +18,13 @@ def _check_shapes_equal(x1, x2):
             "Shapes of both arrays or tensors must be equal. Got shapes: "
             + _shapes
         )
+
+
+def _check_all_x_in_subset_numpy(x, subset=(0, 1)):
+    """Raise `ValueError` if any value of `x` is not in `subset`."""
+    x = np.asarray(x)
+    masks = (np.equal(x, ii) for ii in subset)
+    all_x_in_subset = np.logical_or(*masks).all()
+    if not all_x_in_subset:
+        _subset = ", ".join(map(str, subset))
+        raise ValueError("Not all values are in set {}.".format(_subset))
