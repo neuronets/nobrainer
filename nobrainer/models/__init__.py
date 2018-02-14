ab4dbb18c50a49e8c250b285ae39d698f91eacfe
= <jakubk@mit.edu>
2018-02-14 16:50:24 -0500

    add quicknat and `get_estimator`

--- nobrainer/models/__init__.py
+++ nobrainer/models/__init__.py
@@ -1,2 +1,4 @@
 from nobrainer.models.highres3dnet import HighRes3DNet
 from nobrainer.models.meshnet import MeshNet
+from nobrainer.models.quicknat import QuickNAT
+from nobrainer.models.util import get_estimator
