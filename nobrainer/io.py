34817060c22e59675e872da130383f73b33f77c1
= <jakubk@mit.edu>
2018-02-16 14:06:39 -0500

    minor enhancements

--- nobrainer/io.py
+++ nobrainer/io.py
@@ -7,13 +7,26 @@ import nibabel as nib
 import numpy as np
 import tensorflow as tf
 
+from nobrainer.preprocessing import as_blocks
 
-def load_json(filepath, **kwargs):
+
+def read_json(filepath, **kwargs):
     """Load JSON file `filepath` as dictionary. `kwargs` are keyword arguments
     for `json.load()`.
     """
     with open(filepath, 'r') as fp:
         return json.load(fp, **kwargs)
+
+
+def read_csv(filepath, header=True, delimiter=','):
+    """Return list of tuples from a CSV, where each tuple contains the items
+    in a row.
+    """
+    with open(filepath, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=delimiter)
+        if header:
+            next(reader)  # skip header
+        return [row for row in reader]
 
 
 def save_json(obj, filepath, indent=4, **kwargs):
@@ -23,17 +36,6 @@ def save_json(obj, filepath, indent=4, **kwargs):
     with open(filepath, 'w') as fp:
         json.dump(obj, fp, indent=indent, **kwargs)
         fp.write('\n')
-
-
-def read_feature_label_filepaths(filepath, header=True, delimiter=','):
-    """Return list of tuples from a CSV, where each tuple contains the items
-    in a row.
-    """
-    with open(filepath, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=delimiter)
-        if header:
-            next(reader)  # skip header
-        return [row for row in reader]
 
 
 def load_volume(filepath, dtype=None, return_affine=False):
@@ -46,62 +48,57 @@ def load_volume(filepath, dtype=None, return_affine=False):
     return data if not return_affine else (data, img.affine)
 
 
-def as_blocks(a, block_shape):
-    """Return new array of shape `(n_blocks, *block_shape)`."""
-    orig_shape = np.asarray(a.shape)
-    blocks = orig_shape // block_shape
-    inter_shape = tuple(e for tup in zip(blocks, block_shape) for e in tup)
-    new_shape = (-1,) + block_shape
-    perm = (0, 2, 4, 1, 3, 5)  # TODO: generalize this.
-    return a.reshape(inter_shape).transpose(perm).reshape(new_shape)
+def iterator_nibabel(list_of_filepaths, x_dtype, y_dtype, block_shape,
+                     batch_size):
+    """Yield tuples of numpy arrays `(features, labels)` from a list of
+    filepaths to neuroimaging files.
+    """
+    for idx, (features_fp, labels_fp) in enumerate(list_of_filepaths):
 
+        tf.logging.info("Reading pair number {}".format(idx))
 
-def iter_features_labels_fn_builder(list_of_filepaths, x_dtype, y_dtype,
-                                    block_shape, batch_size):
-    """Return function that yields tuples of `(features, labels)`."""
-
-    def iter_features_labels():
-        """Yield tuples of numpy arrays `(features, labels)`."""
-        for idx, (features_fp, labels_fp) in enumerate(list_of_filepaths):
-
-            tf.logging.info("Reading pair number {}".format(idx))
-
+        try:
             features = load_volume(features_fp, dtype=x_dtype)
-            features = as_blocks(features, block_shape)
-            features = np.expand_dims(features, -1)
-
             labels = load_volume(labels_fp, dtype=y_dtype)
-            labels = as_blocks(labels, block_shape)
+        except Exception:
+            tf.logging.warn(
+                "Error reading at least one input file. Skipping {}, {}"
+                .format(features_fp, labels_fp)
+            )
+            continue
 
-            n_blocks = features.shape[0]
+        features = as_blocks(features, block_shape)
+        features = np.expand_dims(features, -1)
 
-            if batch_size > n_blocks:
-                raise ValueError(
-                    "Batch size must be less than or equal to the number of"
-                    " blocks. Got batch size `{}` and {} blocks"
-                    .format(batch_size, n_blocks)
-                )
+        labels = as_blocks(labels, block_shape)
 
-            iter_range = n_blocks / batch_size
-            if not iter_range.is_integer():
-                raise ValueError(
-                    "Batch size must be a factor of number of blocks. Got"
-                    "batch size `{}` and {} blocks"
-                    .format(batch_size, n_blocks)
-                )
+        n_blocks = features.shape[0]
 
-            # Yield non-overlapping batches of blocks.
-            for ii in range(int(iter_range)):
-                tf.logging.debug("Yielding batch {}".format(ii))
-                _start = int(ii * batch_size)
-                _end = _start + batch_size
-                _slice = slice(_start, _end)
-                yield (
-                    features[_slice, Ellipsis],
-                    labels[_slice, Ellipsis],
-                )
+        if batch_size > n_blocks:
+            raise ValueError(
+                "Batch size must be less than or equal to the number of"
+                " blocks. Got batch size `{}` and {} blocks"
+                .format(batch_size, n_blocks)
+            )
 
-    return iter_features_labels
+        iter_range = n_blocks / batch_size
+        if not iter_range.is_integer():
+            raise ValueError(
+                "Batch size must be a factor of number of blocks. Got"
+                "batch size `{}` and {} blocks"
+                .format(batch_size, n_blocks)
+            )
+
+        # Yield non-overlapping batches of blocks.
+        for ii in range(int(iter_range)):
+            tf.logging.debug("Yielding batch {}".format(ii))
+            _start = int(ii * batch_size)
+            _end = _start + batch_size
+            _slice = slice(_start, _end)
+            yield (
+                features[_slice, Ellipsis],
+                labels[_slice, Ellipsis],
+            )
 
 
 def input_fn_builder(generator, output_types, output_shapes, repeat=None):
--- nobrainer/tests/test_metrics.py
+++ nobrainer/tests/test_metrics.py
@@ -2,23 +2,27 @@
 
 import numpy as np
 
-import nobrainer
+from nobrainer.metrics import dice_coefficient_numpy
 
 
-def test_dice_coefficient():
+def test_dice_coefficient_numpy():
     shape = (2, 2, 2)
     x1 = np.ones(shape)
     x2 = np.ones(shape)
 
-    dice = nobrainer.metrics.dice_coefficient_numpy(x1, x2, reducer=None)
+    dice = dice_coefficient_numpy(x1, x2, reducer=None)
     assert np.array_equal(dice, [1, 1])
 
-    dice = nobrainer.metrics.dice_coefficient_numpy(x1, x2, reducer=np.mean)
+    dice = dice_coefficient_numpy(x1, x2, reducer=np.mean)
     assert dice == 1
 
     x1[0, :, 0] = 0
-    dice = nobrainer.metrics.dice_coefficient_numpy(x1, x2, reducer=None)
+    dice = dice_coefficient_numpy(x1, x2, reducer=None)
     assert np.array_equal(dice, [2 / 3, 1])
 
-    dice = nobrainer.metrics.dice_coefficient_numpy(x1, x2, reducer=np.mean)
+    dice = dice_coefficient_numpy(x1, x2, reducer=np.mean)
     assert np.array_equal(dice, np.mean((2 / 3, 1)))
+
+    x1 = np.zeros(shape)
+    x2 = np.zeros(shape)
+    assert np.isnan(dice_coefficient_numpy(x1, x2))
