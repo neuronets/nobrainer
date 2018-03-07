92823a4bff35914580b981604c497c8b98dd859f
= <jakubk@mit.edu>
2018-03-07 17:50:20 -0500

    add+fix: multi gpu option + epoch generator bug

--- train_on_hdf5.py
+++ train_on_hdf5.py
@@ -30,6 +30,8 @@ def write_params_to_file(filepath, params):
     if os.path.isfile(filepath):
         previous_params = nobrainer.io.read_json(filepath)
         to_save.update(previous_params)
+        if params['model_dir'] in previous_params:
+            return  # do not overwrite existing entries.
     nobrainer.io.save_json(to_save, filepath)
 
 
@@ -86,7 +88,8 @@ def iter_hdf5(filepath, x_dataset, y_dataset, x_dtype, y_dtype,
             features = fp[x_dataset][start:end]
             labels = fp[y_dataset][start:end]
 
-        features = nobrainer.preprocessing.normalize_zero_one(features)
+        # Data are normalized in the hdf5 file.
+        # features = nobrainer.preprocessing.normalize_zero_one(features)
 
         features = np.expand_dims(features, -1)
         features = features.astype(x_dtype)
@@ -99,6 +102,9 @@ def iter_hdf5(filepath, x_dataset, y_dataset, x_dtype, y_dtype,
 
         if binarize_y:
             labels = nobrainer.preprocessing.binarize(labels, threshold=0)
+
+        print("FEATURES SHAPE:", features.shape)
+        print("LABELS SHAPE:", labels.shape, flush=True)
 
         yield features, labels
 
@@ -114,6 +120,10 @@ def train(params):
         .format(x=x_dataset, y=y_dataset)
     )
 
+    with h5py.File(params['filepath'], mode='r') as fp:
+        examples_per_epoch = fp[x_dataset].shape[0]
+        assert examples_per_epoch == fp[y_dataset].shape[0]
+
     if params['aparcaseg_mapping']:
         tf.logging.info(
             "Reading mapping file: {}".format(params['aparcaseg_mapping'])
@@ -122,7 +132,7 @@ def train(params):
     else:
         mapping = None
 
-    generator = iter_hdf5(
+    generator_builder = lambda: iter_hdf5(
         filepath=params['filepath'],
         x_dataset=x_dataset,
         y_dataset=y_dataset,
@@ -140,10 +150,13 @@ def train(params):
     )
 
     input_fn = nobrainer.io.input_fn_builder(
-        generator=lambda: generator,
+        generator=generator_builder,
         output_types=(_DT_X_TF, _DT_Y_TF),
         output_shapes=_output_shapes,
-        repeat=params['repeat'],
+        num_epochs=params['num_epochs'],
+        multi_gpu=params['multi_gpu'],
+        examples_per_epoch=examples_per_epoch,
+        batch_size=params['batch_size'],
     )
 
     runconfig = tf.estimator.RunConfig(
@@ -156,6 +169,7 @@ def train(params):
         model_dir=params['model_dir'],
         config=runconfig,
         learning_rate=params['learning_rate'],
+        multi_gpu=params['multi_gpu'],
     )
 
     write_params_to_file(
@@ -176,7 +190,9 @@ def create_parser():
     p.add_argument('--brainmask', action='store_true')
     p.add_argument('--aparcaseg-mapping')
     p.add_argument('--filepath', required=True, type=str)
-    p.add_argument('--repeat', type=int, default=None)
+    p.add_argument('-e', '--num-epochs', type=int, default=1)
+    p.add_argument('--multi-gpu', action='store_true')
+    p.add_argument('--model-dir')
     return p
 
 
@@ -199,6 +215,7 @@ if __name__ == '__main__':
 
     params['block_shape'] = tuple(params['block_shape'])
 
-    params['model_dir'] = get_model_dir(BASE_MODEL_SAVE_PATH, params['model'])
+    if params['model_dir'] is None:
+        params['model_dir'] = get_model_dir(BASE_MODEL_SAVE_PATH, params['model'])
 
     train(params)
