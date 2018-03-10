bae14af58a29814f9052bf411ba02aee3eaf39ad
= <jakubk@mit.edu>
2018-03-09 20:35:28 -0500

    add methods to validate params

--- nobrainer/models/util.py
+++ nobrainer/models/util.py
@@ -30,3 +30,28 @@ def get_estimator(name):
             "Model is not available: {}. Available models are {}."
             .format(name, avail)
         )
+
+
+def get_items_not_in_iterable(items, iterable):
+    """Return set of items not in `iterable`."""
+    return {i for i in items if i not in iterable}
+
+
+def check_required_params(required_keys, params):
+    """Raise `ValueError` if any items in `required_keys` are not in dictionary
+    `params`.
+    """
+    notin = get_items_not_in_iterable(required_keys, params)
+    if notin:
+        raise ValueError(
+            "required parameters were not found: '{}'."
+            .format("', ".join(notin))
+        )
+
+
+def set_default_params(defaults, params):
+    """Insert keys and values from `defaults` into dictionary `params` if those
+    keys do not exist in `params`. Modifies `params` in-place.
+    """
+    for k, v in defaults.items():
+        params.setdefault(k, v)
