dfc54713dfb66356e2d63f577c7f619bda6c6c22
= <jakubk@mit.edu>
2018-03-20 18:05:37 -0400

    enhancements

--- predict.py
+++ predict.py
@@ -1,63 +1,79 @@
 #!/usr/bin/env python3
+"""Commandline script to predict on T1 volumes."""
 
-import nibabel as nib
+import argparse
+import sys
+
+import nibabel as nb
 import numpy as np
 import tensorflow as tf
 
-import nobrainer
+from nobrainer.models import HighRes3DNet
+from nobrainer.preprocessing import as_blocks
+from nobrainer.preprocessing import from_blocks
+from nobrainer.preprocessing import normalize_zero_one
 
 
-def from_blocks(a, output_shape):
-    """Initial implementation of function to go from blocks to full volume."""
-    return (
-        a.reshape((2, 2, 2, 128, 128, 128))
-        .transpose((0, 3, 1, 4, 2, 5))
-        .reshape(output_shape))
+def predict(features_filepath, batch_size, block_shape):
+    """Run `model_obj.predict` on features data in `features_filepath`."""
+    x_img = nb.load(features_filepath)
 
+    x = np.asarray(x_img.dataobj).astype(np.float32)
+    x = as_blocks(x, block_shape)
+    x = normalize_zero_one(x)
+    x = x[..., np.newaxis]
 
-def predict(filepath):
-    """Return Nibabel Nifti image of predicted brainmask for neuroimaging file
-    `filepath`.
-    """
-    x, x_affine = nobrainer.io.load_volume(
-        filepath, dtype='float32', return_affine=True
-    )
+    input_fn = tf.estimator.inputs.numpy_input_fn(
+        x=x, shuffle=False, batch_size=batch_size)
 
-    assert x.shape == (256, 256, 256), "shape must be (256, 256, 256)."
+    # Load model.
+    # TODO(kaczmarj): use tensorflow serving or the export api instead of this.
+    # also these shouldn't be hardcoded.
+    model = HighRes3DNet(
+        n_classes=2,
+        model_dir="/opt/nobrainer/trained-models/highres3dnet-brainmask")
 
-    x = nobrainer.preprocessing.as_blocks(x, (128, 128, 128))
-    x = np.expand_dims(x, -1)
-    x = nobrainer.preprocessing.normalize_zero_one(x)
-
-    inputs = tf.estimator.inputs.numpy_input_fn(
-        x=x, shuffle=False, batch_size=4
-    )
-
-    model = nobrainer.models.MeshNet(
-        2, model_dir='models-hdf5/meshnet_20180219-220005'
-    )
-
-    generator = model.predict(input_fn=inputs)
-    predictions = np.zeros((8, 128, 128, 128))
-
+    generator = model.predict(input_fn=input_fn)
+    predictions = np.zeros(x.shape[:-1])
     for ii, block in enumerate(generator):
-        predictions[ii, Ellipsis] = block
+        predictions[ii, Ellipsis] = block['class_ids']
+    predictions = from_blocks(predictions, x_img.shape)
 
-    predictions = from_blocks(predictions, (256, 256, 256))
+    return nb.Nifti1Image(
+        predictions, affine=x_img.affine, header=x_img.header)
 
-    return nib.Nifti1Image(predictions, x_affine)
+
+def create_parser():
+    """Return argument parser."""
+    p = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    p.add_argument('input', help="path to T1 on which to predict")
+    p.add_argument('output', help="path to save predictions")
+    p.add_argument(
+        '--batch-size', type=int, default=4,
+        help="Batch size. Use lower value if resources are limited")
+    p.add_argument(
+        '--block-shape', nargs=3, type=int, default=[128, 128, 128],
+        help="Separate input volume into 3D blocks of this shape before"
+             " training")
+    return p
+
+
+def parse_args(args):
+    """Return namespace of arguments."""
+    parser = create_parser()
+    return parser.parse_args(args)
 
 
 if __name__ == '__main__':
-    import sys
+    namespace = parse_args(sys.argv[1:])
+    params = vars(namespace)
 
-    if len(sys.argv) < 3:
-        raise ValueError("input and output filepaths must be specified.")
+    params['block_shape'] = tuple(params['block_shape'])
 
-    input_filepath = sys.argv[1]
-    output_filepath = sys.argv[2]
+    predicted_img = predict(
+        params['input'],
+        batch_size=params['batch_size'],
+        block_shape=params['block_shape'])
 
-    brainmask_nifti = predict(input_filepath)
-
-    brainmask_nifti.to_filename(output_filepath)
-
+    nb.save(predicted_img, params['output'])
