689b64d183720ff6c544bc20e25f705c90758c8a
= <jakubk@mit.edu>
2018-03-27 09:21:10 -0400

    add options for dropout and removing batchnorm

--- nobrainer/models/highres3dnet_test.py
+++ nobrainer/models/highres3dnet_test.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 import nobrainer
 
 
-def test_meshnet():
+def test_highres3dnet():
     shape = (1, 10, 10, 10)
     X = np.random.rand(*shape, 1).astype(np.float32)
     y = np.random.randint(0, 9, size=(shape), dtype=np.int32)
@@ -22,4 +22,10 @@ def test_meshnet():
     optimizer = tf.train.AdagradOptimizer(learning_rate=0.001)
     estimator = nobrainer.models.HighRes3DNet(
         n_classes=10, optimizer=optimizer, learning_rate=0.001)
+    estimator.train(input_fn=dset_fn)
+
+    # With one batchnorm layer per residually connected pair and dropout.
+    estimator = nobrainer.models.HighRes3DNet(
+        n_classes=10, optimizer='Adam', learning_rate=0.001,
+        one_batchnorm_per_resblock=True, dropout_rate=0.25)
     estimator.train(input_fn=dset_fn)
