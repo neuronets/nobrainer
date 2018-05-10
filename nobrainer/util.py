e439f50cb15fdef53b06d775f6477c4973058688
Jakub Kaczmarzyk <jakubk@mit.edu>
2018-05-09 21:44:50 -0400

    move methods to nobrainer.volume

--- nobrainer/util.py
+++ nobrainer/util.py
@@ -1,14 +1,10 @@
 """Utilities."""
 
-import copy
-import numbers
 import random
 
 import h5py
 import numpy as np
 import tensorflow as tf
-
-from nobrainer.io import load_volume
 
 
 def _shapes_equal(x1, x2):
@@ -53,119 +49,6 @@ def create_indices(num_samples, batch_size, shuffle=False):
         random.shuffle(indices)
 
     return indices
-
-
-def _get_n_blocks(arr_shape, kernel_size, strides=1):
-    """Return number of blocks that will result from sliding a kernel across
-    input array with a certain stride.
-    """
-    def get_n(a, k, s):
-        """Return number of blocks that result from length `a` of input
-        array, length `k` of kernel, and stride `s`."""
-        return (a - k) / s + 1
-
-    arr_ndim = len(arr_shape)
-
-    if isinstance(kernel_size, numbers.Number):
-        kernel_size = [kernel_size] * arr_ndim
-    if isinstance(strides, numbers.Number):
-        strides = [strides] * arr_ndim
-
-    err = "Length of `{}` must be equal to the number of dimensions in `arr`."
-    if len(kernel_size) != arr_ndim:
-        raise ValueError(err.format('kernel_size'))
-    if len(strides) != arr_ndim:
-        raise ValueError(err.format('strides'))
-
-    n_blocks = tuple(
-        get_n(aa, kk, ss) for aa, kk, ss
-        in zip(arr_shape, kernel_size, strides))
-
-    for n in n_blocks:
-        if not n.is_integer() or n < 1:
-            raise ValueError(
-                "Invalid combination of input shape, kernel size, and strides."
-                " This combination would create a non-integer or <1 number of"
-                " blocks.")
-
-    return tuple(map(int, n_blocks))
-
-
-def iterblocks_2d(arr, kernel_size, strides=(1, 1)):
-    """Yield blocks of 2D array."""
-    if arr.ndim != 2:
-        raise ValueError("Input array must be 2D.")
-
-    bx, by = _get_n_blocks(
-        arr_shape=arr.shape, kernel_size=kernel_size, strides=strides)
-
-    for ix in range(bx):
-        ix *= strides[0]
-        for iy in range(by):
-            iy *= strides[1]
-            ixs = slice(ix, ix + kernel_size[0])
-            iys = slice(iy, iy + kernel_size[1])
-            yield arr[ixs, iys]
-
-
-def iterblocks_3d(arr, kernel_size, strides=(1, 1, 1)):
-    """Yield blocks of 3D array."""
-    if arr.ndim != 3:
-        raise ValueError("Input array must be 3D.")
-
-    bx, by, bz = _get_n_blocks(
-        arr_shape=arr.shape, kernel_size=kernel_size, strides=strides)
-
-    for ix in range(bx):
-        ix *= strides[0]
-        for iy in range(by):
-            iy *= strides[1]
-            for iz in range(bz):
-                iz *= strides[2]
-                ixs = slice(ix, ix + kernel_size[0])
-                iys = slice(iy, iy + kernel_size[1])
-                izs = slice(iz, iz + kernel_size[2])
-                yield arr[ixs, iys, izs]
-
-
-def iter_volumes(list_of_filepaths, vol_shape, block_shape, x_dtype, y_dtype,
-                 strides=(1, 1, 1), shuffle=False, normalizer=None):
-    """Yield tuples of numpy arrays `(features, labels)` from a list of
-    filepaths to neuroimaging files.
-    """
-    list_of_filepaths = copy.deepcopy(list_of_filepaths)
-    if shuffle:
-        random.shuffle(list_of_filepaths)
-
-    _n_blocks = np.prod(
-        _get_n_blocks(
-            arr_shape=vol_shape, kernel_size=block_shape, strides=strides))
-
-    tf.logging.info(
-        "Will yield {} blocks of shape {} per volume"
-        .format(_n_blocks, block_shape))
-
-    for idx, (features_fp, labels_fp) in enumerate(list_of_filepaths):
-        try:
-            features = load_volume(features_fp, dtype=x_dtype)
-            labels = load_volume(labels_fp, dtype=y_dtype)
-        except Exception:
-            tf.logging.fatal(
-                "Error reading at least one input file: {} {}"
-                .format(features_fp, labels_fp))
-            raise
-
-        if normalizer is not None:
-            features, labels = normalizer(features, labels)
-
-        _check_shapes_equal(features, labels)
-        feature_gen = iterblocks_3d(
-            arr=features, kernel_size=block_shape, strides=strides)
-        label_gen = iterblocks_3d(
-            arr=labels, kernel_size=block_shape, strides=strides)
-
-        for ff, ll in zip(feature_gen, label_gen):
-            yield ff[..., np.newaxis], ll
 
 
 def iter_hdf5(filepath, x_dataset, y_dataset, x_dtype, y_dtype, shuffle=False,
