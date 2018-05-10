d7bcef5aba4b24ddbbe9bb1873e1b6b77cac4fea
Jakub Kaczmarzyk <jakubk@mit.edu>
2018-05-09 21:47:02 -0400

    add methods for training

---
+++ nobrainer/train.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""Methods to train models."""
+
+import numpy as np
+import tensorflow as tf
+
+from nobrainer.volume import _get_n_blocks
+
+# Data types of features (x) and labels (y).
+DT_X = "float32"
+DT_Y = "int32"
+
+
+def train(model,
+          volume_data_generator,
+          filepaths,
+          volume_shape,
+          block_shape,
+          strides,
+          x_dtype=DT_X,
+          y_dtype=DT_Y,
+          shuffle=True,
+          batch_size=8,
+          num_epochs=1,
+          prefetch=1,
+          multi_gpu=False,
+          eval_volume_data_generator=None,
+          eval_filepaths=None):
+    """"""
+
+    input_fn = volume_data_generator.dset_input_fn_builder(
+        filepaths=filepaths,
+        volume_shape=volume_shape,
+        block_shape=block_shape,
+        strides=strides,
+        x_dtype=DT_X,
+        y_dtype=DT_Y,
+        shuffle=shuffle,
+        batch_size=batch_size,
+        num_epochs=num_epochs,
+        prefetch=prefetch,
+        multi_gpu=multi_gpu)
+
+    if eval_volume_data_generator is None:
+        model.train(input_fn=input_fn)
+
+    else:
+        if eval_filepaths is None:
+            raise ValueError(
+                "must specify `eval_filepaths` if `eval_volume_data_generator`"
+                " is used.")
+        eval_input_fn = eval_volume_data_generator.dset_input_fn_builder(
+            filepaths=eval_filepaths,
+            volume_shape=volume_shape,
+            block_shape=block_shape,
+            strides=strides,
+            x_dtype=DT_X,
+            y_dtype=DT_Y,
+            batch_size=batch_size,
+            num_epochs=num_epochs,
+            prefetch=prefetch,
+            multi_gpu=multi_gpu)
+
+        examples_per_volume = np.prod(
+            _get_n_blocks(
+                arr_shape=volume_shape,
+                kernel_size=block_shape,
+                strides=strides))
+        n_samples_per_epoch = examples_per_volume * len(filepaths)
+        max_steps = n_samples_per_epoch * num_epochs
+
+        tf.logging.info("Will train for {} steps".format(max_steps))
+        train_spec = tf.estimator.TrainSpec(
+            input_fn=input_fn,
+            max_steps=max_steps,
+            hooks=None)
+
+        eval_spec = tf.estimator.EvalSpec(
+            input_fn=eval_input_fn,
+            steps=None,  # Evaluate until input_fn raises end-of-input.
+            name=None,
+            hooks=None,
+            exporters=None,
+            start_delay_secs=600,  # Start evaluating after 10 minutes.
+            throttle_secs=1200)  # Evaluate every 20 minutes.
+
+        tf.estimator.train_and_evaluate(
+            estimator=model,
+            train_spec=train_spec,
+            eval_spec=eval_spec)
