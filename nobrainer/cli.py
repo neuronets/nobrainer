d88e8fb0f44a73748f95df97e637b55e083a508d
leej3 <johnleenimh@gmail.com>
2018-08-06 14:33:30 -0400

    add validate function to cli

--- nobrainer/cli.py
+++ nobrainer/cli.py
@@ -278,16 +278,6 @@ def predict(params):
     if params["n_samples"]  < 1:
         raise Exception('n-samples cannot be lower than 1.')
 
-    ###
-    def nameFixer(thePath, newTxt):
-        arr = thePath.split(".")
-        if len(arr)==0:
-            return newTxt
-        txt = arr[0] + "_" + newTxt
-        for i in range(1, len(arr)):
-            txt = txt + "." + arr[i]
-        return txt
-    
     meanPath = Path(nameFixer(params['output'], "means") )
     variancePath = Path(nameFixer(params['output'], "variance"))
     entropyPath = Path(nameFixer(params['output'], "entropy"))
@@ -307,6 +297,77 @@ def predict(params):
         n_samples=params['n_samples'],
         batch_size=params['batch_size'])
     if not rA:
+        includeVariance = ((params['n_samples'] > 1) and (rV))
+        returnEntropy = rE
+        if includeVariance:
+            if returnEntropy:
+                nib.save(imgs[0], str(meanPath))
+                nib.save(imgs[1], str(variancePath))
+                nib.save(imgs[2], str(entropyPath))
+            else:
+                nib.save(imgs[0], str(meanPath))
+                nib.save(imgs[1], str(variancePath))
+        else:
+            if returnEntropy:
+                nib.save(imgs[0], str(meanPath))
+                nib.save(imgs[1], str(entropyPath))
+            else:
+                nib.save(imgs[0], str(meanPath))
+
+
+
+def validate(params):
+    ### add normalizer too!
+    
+    rV = params['returnVariance']
+    rE = params['returnEntropy']
+    rA = params['returnArrayFromImages']
+
+    normalizer = None
+    sm = params["samplewise_minmax"]
+    sz = params["samplewise_zscore"]
+    if sm and sz:
+        raise Exception("Normalizer cannot be both minmax and zscore")
+    if sm:
+        normalizer = normalize_zero_one
+    if sz:
+        normalizer = zscore
+
+    if params["n_samples"]  < 1:
+        raise Exception('n-samples cannot be lower than 1.')
+
+        
+
+    
+    imgs = _validate_from_filepaths(
+        inputs=params['input'],
+        predictor=params['model'],
+        n_classes = params['n_classes'],
+        mapping_y = params['label_mapping'],
+
+        block_shape=params['block_shape'],
+        returnVariance=rV,
+        returnEntropy=rE,
+        returnArrayFromImages=rA,
+        normalizer=normalizer,
+        n_samples=params['n_samples'],
+        batch_size=params['batch_size'])
+
+    for image in imgs:
+        print
+        save
+        
+
+
+    if not rA:
+    
+
+        meanPath = 
+        variancePath = Path(nameFixer(params['output'], "variance"))
+        entropyPath = Path(nameFixer(params['output'], "entropy"))
+
+        if meanPath.is_file() or variancePath.is_file() or entropyPath.is_file():
+            raise Exception(str(meanPath) + " or " + str(variancePath) + " or " + str(entropyPath) + " already exists.")
         includeVariance = ((params['n_samples'] > 1) and (rV))
         returnEntropy = rE
         if includeVariance:
