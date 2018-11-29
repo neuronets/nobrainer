9504205f0910fa4a9b1c9fccf231edc1c2ab9567
Jakub Kaczmarzyk <jakubk@mit.edu>
2018-11-29 13:47:26 -0500

    Update nobrainer/cli.py
    Co-Authored-By: leej3 <leej3@users.noreply.github.com>

--- nobrainer/cli.py
+++ nobrainer/cli.py
@@ -147,7 +147,7 @@ def create_parser():
         help="Number of sampling.")
     ppp.add_argument('--return-entropy', action='store_true',
         help = 'if you want to return entropy, add this flag.')
-    ppp.add_argument('--return_variance', action='store_true', 
+    ppp.add_argument('--return-variance', action='store_true', 
         help ='if you want to return variance, add this flag.')
     ppp.add_argument('--return_array_from_images', action = 'store_true', 
         help = 'if you want to return array instead of image, add this flag.')
