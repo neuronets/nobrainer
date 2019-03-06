ab4578b70074d73e90292871f9662909579ff0fb
Jakub Kaczmarzyk <jakubk@mit.edu>
2019-03-06 14:57:06 -0500

    do not use batchnorm and dropout + enh docs

--- nobrainer/models/meshnet.py
+++ nobrainer/models/meshnet.py
@@ -6,7 +6,7 @@ Implemented according to the [MeshNet manuscript](https://arxiv.org/abs/1612.009
 import tensorflow as tf
 from tensorflow.keras import layers
 
-from nobrainer.layers.variational_convolution import VariationalConv3D
+from nobrainer.layers.variational_convolution import VWNConv3D
 
 
 def meshnet(n_classes, input_shape, receptive_field=67, filters=71, activation='relu', dropout_rate=0.25, batch_size=None, name='meshnet'):
@@ -78,16 +78,18 @@ def meshnet(n_classes, input_shape, receptive_field=67, filters=71, activation='
 
     x = layers.Conv3D(filters=n_classes, kernel_size=(1, 1, 1), padding='same', name='classification/conv3d')(x)
 
-    final_activation = 'sigmoid' if n_classes <= 2 else 'softmax'
+    final_activation = 'sigmoid' if n_classes == 1 else 'softmax'
     x = layers.Activation(final_activation, name='classification/activation')(x)
 
     return tf.keras.Model(inputs=inputs, outputs=x, name=name)
 
 
-def meshnet_vwn(n_classes, input_shape, receptive_field=67, filters=71, activation='relu', dropout_rate=0.25, is_mc=False, batch_size=None, name='meshnet_vwn'):
-    """Instantiate variational weight MeshNet model.
+def meshnet_vwn(n_classes, input_shape, receptive_field=67, filters=71, activation='relu', is_mc=False, batch_size=None, name='meshnet_vwn'):
+    """Instantiate variational MeshNet model, which also uses weight
+    normalization.
 
     Please see https://arxiv.org/abs/1805.10863 for more information.
+
 
     Parameters
     ----------
@@ -102,9 +104,10 @@ def meshnet_vwn(n_classes, input_shape, receptive_field=67, filters=71, activati
         MeshNet manuscript uses 21 filters for a binary segmentation task
         (i.e., brain extraction) and 71 filters for a multi-class segmentation task.
     activation: str or optimizer object, the non-linearity to use.
-    dropout_rate: float between 0 and 1, the fraction of input units to drop.
-    is_mc: bool, whether to apply variational dropout in the variational
-        convolution layers.
+    is_mc: bool, if true, apply variational approximation in the variational
+        convolution layers. If false, will use the most likely weight (i.e.,
+        the mean of the weight gaussian) instead of sampling a weight from the
+        gaussian distribution.
     batch_size: int, number of samples in each batch. This must be set when
         training on TPUs.
     name: str, name to give to the resulting model object.
@@ -122,10 +125,14 @@ def meshnet_vwn(n_classes, input_shape, receptive_field=67, filters=71, activati
         raise ValueError("unknown receptive field. Legal values are 37, 67, and 129.")
 
     def one_layer(x, layer_num, dilation_rate=(1, 1, 1)):
-        x = VariationalConv3D(filters, kernel_size=(3, 3, 3), padding='same', dilation_rate=dilation_rate, is_mc=is_mc, name='layer{}/vconv3d'.format(layer_num))(x)
-        x = layers.BatchNormalization(name='layer{}/batchnorm'.format(layer_num))(x)
+        # Normally, meshnet has layers of
+        # conv -> batchnorm --> activation --> dropout
+        # but for the variational model, we do not include batchnorm and
+        # dropout. The variational convolution implements weightnorm, which
+        # gives similar benefits as batchnorm without the extra trainable
+        # parameters.
+        x = VWNConv3D(filters, kernel_size=(3, 3, 3), padding='same', dilation_rate=dilation_rate, is_mc=is_mc, name='layer{}/vwnconv3d'.format(layer_num))(x)
         x = layers.Activation(activation, name='layer{}/activation'.format(layer_num))(x)
-        x = layers.Dropout(dropout_rate, name='layer{}/dropout'.format(layer_num))(x)
         return x
 
     inputs = layers.Input(shape=input_shape, batch_size=batch_size, name='inputs')
@@ -155,9 +162,9 @@ def meshnet_vwn(n_classes, input_shape, receptive_field=67, filters=71, activati
         x = one_layer(x, 6, dilation_rate=(32, 32, 32))
         x = one_layer(x, 7)
 
-    x = VariationalConv3D(filters=n_classes, kernel_size=(1, 1, 1), padding='same', is_mc=is_mc, name='classification/vconv3d')(x)
+    x = VWNConv3D(filters=n_classes, kernel_size=(1, 1, 1), padding='same', is_mc=is_mc, name='classification/vwnconv3d')(x)
 
-    final_activation = 'sigmoid' if n_classes <= 2 else 'softmax'
+    final_activation = 'sigmoid' if n_classes == 1 else 'softmax'
     x = layers.Activation(final_activation, name='classification/activation')(x)
 
     return tf.keras.Model(inputs=inputs, outputs=x, name=name)
