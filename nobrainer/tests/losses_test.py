d05a5d991d4429d73e682765511f5fca76d7e713
Jakub Kaczmarzyk <jakubk@mit.edu>
2019-03-18 19:00:46 -0400

    use numpy testing funcs

--- nobrainer/tests/losses_test.py
+++ nobrainer/tests/losses_test.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numpy.testing import assert_allclose
+from numpy.testing import assert_array_equal
 import pytest
 import scipy.spatial.distance
 import tensorflow as tf
@@ -10,25 +12,25 @@ def test_dice():
     x = np.zeros(4)
     y = np.zeros(4)
     out = losses.dice(x, y, axis=None).numpy()
-    assert np.allclose(out, 0)
+    assert_allclose(out, 0)
 
     x = np.ones(4)
     y = np.ones(4)
     out = losses.dice(x, y, axis=None).numpy()
-    assert np.allclose(out, 0)
+    assert_allclose(out, 0)
 
     x = [0., 0., 1., 1.]
     y = [1., 1., 1., 1.]
     out = losses.dice(x, y, axis=None).numpy()
     ref = scipy.spatial.distance.dice(x, y)
-    assert np.allclose(out, ref)
+    assert_allclose(out, ref)
 
     x = [0., 0., 1., 1.]
     y = [1., 1., 0., 0.]
     out = losses.dice(x, y, axis=None).numpy()
     ref = scipy.spatial.distance.dice(x, y)
-    assert np.allclose(out, ref)
-    assert np.allclose(out, 1)
+    assert_allclose(out, ref)
+    assert_allclose(out, 1)
 
     x = np.ones((4, 32, 32, 32, 1), dtype=np.float32)
     y = x.copy()
@@ -38,29 +40,29 @@ def test_dice():
     dices = np.empty(x.shape[0])
     for i in range(x.shape[0]):
         dices[i] = scipy.spatial.distance.dice(x[i].flatten(), y[i].flatten())
-    assert np.allclose(losses.dice(x, y, axis=(1, 2, 3, 4)), dices)
-    assert np.allclose(losses.Dice(axis=(1, 2, 3, 4))(x, y), dices.mean())
-    assert np.allclose(losses.Dice(axis=(1, 2, 3, 4))(y, x), dices.mean())
+    assert_allclose(losses.dice(x, y, axis=(1, 2, 3, 4)), dices)
+    assert_allclose(losses.Dice(axis=(1, 2, 3, 4))(x, y), dices.mean())
+    assert_allclose(losses.Dice(axis=(1, 2, 3, 4))(y, x), dices.mean())
 
 
 def test_generalized_dice():
     shape = (8, 32, 32, 32, 16)
     x = np.zeros(shape)
     y = np.zeros(shape)
-    assert np.array_equal(losses.generalized_dice(x, y), np.zeros(shape[0]))
+    assert_array_equal(losses.generalized_dice(x, y), np.zeros(shape[0]))
 
     shape = (8, 32, 32, 32, 16)
     x = np.ones(shape)
     y = np.ones(shape)
-    assert np.array_equal(losses.generalized_dice(x, y), np.zeros(shape[0]))
+    assert_array_equal(losses.generalized_dice(x, y), np.zeros(shape[0]))
 
     shape = (8, 32, 32, 32, 16)
     x = np.ones(shape)
     y = np.zeros(shape)
     # Why aren't the losses exactly one? Could it be the propogation of floating
     # point inaccuracies when summing?
-    assert np.allclose(losses.generalized_dice(x, y), np.ones(shape[0]), atol=1e-03)
-    assert np.allclose(losses.GeneralizedDice(axis=(1, 2, 3))(x, y), losses.generalized_dice(x, y))
+    assert_allclose(losses.generalized_dice(x, y), np.ones(shape[0]), atol=1e-03)
+    assert_allclose(losses.GeneralizedDice(axis=(1, 2, 3))(x, y), losses.generalized_dice(x, y))
 
     x = np.ones((4, 32, 32, 32, 1), dtype=np.float64)
     y = x.copy()
@@ -71,32 +73,32 @@ def test_generalized_dice():
     # makes the generalized form slightly different from Dice.
     gd = losses.generalized_dice(x, y, axis=(1, 2, 3)).numpy()
     dd = losses.dice(x, y, axis=(1, 2, 3, 4)).numpy()
-    assert np.allclose(gd, dd, rtol=1e-02)  # is this close enough?
+    assert_allclose(gd, dd, rtol=1e-02)  # is this close enough?
 
 
 def test_jaccard():
     x = np.zeros(4)
     y = np.zeros(4)
     out = losses.jaccard(x, y, axis=None).numpy()
-    assert np.allclose(out, 0)
+    assert_allclose(out, 0)
 
     x = np.ones(4)
     y = np.ones(4)
     out = losses.jaccard(x, y, axis=None).numpy()
-    assert np.allclose(out, 0)
+    assert_allclose(out, 0)
 
     x = [0., 0., 1., 1.]
     y = [1., 1., 1., 1.]
     out = losses.jaccard(x, y, axis=None).numpy()
     ref = scipy.spatial.distance.jaccard(x, y)
-    assert np.allclose(out, ref)
+    assert_allclose(out, ref)
 
     x = [0., 0., 1., 1.]
     y = [1., 1., 0., 0.]
     out = losses.jaccard(x, y, axis=None).numpy()
     ref = scipy.spatial.distance.jaccard(x, y)
-    assert np.allclose(out, ref)
-    assert np.allclose(out, 1)
+    assert_allclose(out, ref)
+    assert_allclose(out, 1)
 
     x = np.ones((4, 32, 32, 32, 1), dtype=np.float32)
     y = x.copy()
@@ -106,9 +108,9 @@ def test_jaccard():
     jaccards = np.empty(x.shape[0])
     for i in range(x.shape[0]):
         jaccards[i] = scipy.spatial.distance.jaccard(x[i].flatten(), y[i].flatten())
-    assert np.allclose(losses.jaccard(x, y, axis=(1, 2, 3, 4)), jaccards)
-    assert np.allclose(losses.Jaccard(axis=(1, 2, 3, 4))(x, y), jaccards.mean())
-    assert np.allclose(losses.Jaccard(axis=(1, 2, 3, 4))(y, x), jaccards.mean())
+    assert_allclose(losses.jaccard(x, y, axis=(1, 2, 3, 4)), jaccards)
+    assert_allclose(losses.Jaccard(axis=(1, 2, 3, 4))(x, y), jaccards.mean())
+    assert_allclose(losses.Jaccard(axis=(1, 2, 3, 4))(y, x), jaccards.mean())
 
 
 @pytest.mark.xfail
--- nobrainer/tests/metrics_test.py
+++ nobrainer/tests/metrics_test.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numpy.testing import assert_allclose
+from numpy.testing import assert_array_equal
 import pytest
 import scipy.spatial.distance
 import tensorflow as tf
@@ -13,27 +15,27 @@ def test_dice():
     x = np.zeros(4)
     y = np.zeros(4)
     out = metrics.dice(x, y, axis=None).numpy()
-    assert np.allclose(out, 1)
+    assert_allclose(out, 1)
 
     x = np.ones(4)
     y = np.ones(4)
     out = metrics.dice(x, y, axis=None).numpy()
-    assert np.allclose(out, 1)
+    assert_allclose(out, 1)
 
     x = [0., 0., 1., 1.]
     y = [1., 1., 1., 1.]
     out = metrics.dice(x, y, axis=None).numpy()
     ref = 1. - scipy.spatial.distance.dice(x, y)
-    assert np.allclose(out, ref)
+    assert_allclose(out, ref)
     jac_out = metrics.jaccard(x, y, axis=None).numpy()
-    assert np.allclose(out, 2. * jac_out / (1. + jac_out))
+    assert_allclose(out, 2. * jac_out / (1. + jac_out))
 
     x = [0., 0., 1., 1.]
     y = [1., 1., 0., 0.]
     out = metrics.dice(x, y, axis=None).numpy()
     ref = 1. - scipy.spatial.distance.dice(x, y)
-    assert np.allclose(out, ref, atol=1e-07)
-    assert np.allclose(out, 0, atol=1e-07)
+    assert_allclose(out, ref, atol=1e-07)
+    assert_allclose(out, 0, atol=1e-07)
 
     x = np.ones((4, 32, 32, 32, 1), dtype=np.float32)
     y = x.copy()
@@ -43,26 +45,26 @@ def test_dice():
     dices = np.empty(x.shape[0])
     for i in range(x.shape[0]):
         dices[i] = 1. - scipy.spatial.distance.dice(x[i].flatten(), y[i].flatten())
-    assert np.allclose(metrics.dice(x, y, axis=(1, 2, 3, 4)), dices)
+    assert_allclose(metrics.dice(x, y, axis=(1, 2, 3, 4)), dices)
 
 
 def test_generalized_dice():
     shape = (8, 32, 32, 32, 16)
     x = np.zeros(shape)
     y = np.zeros(shape)
-    assert np.array_equal(metrics.generalized_dice(x, y), np.ones(shape[0]))
+    assert_array_equal(metrics.generalized_dice(x, y), np.ones(shape[0]))
 
     shape = (8, 32, 32, 32, 16)
     x = np.ones(shape)
     y = np.ones(shape)
-    assert np.array_equal(metrics.generalized_dice(x, y), np.ones(shape[0]))
+    assert_array_equal(metrics.generalized_dice(x, y), np.ones(shape[0]))
 
     shape = (8, 32, 32, 32, 16)
     x = np.ones(shape)
     y = np.zeros(shape)
     # Why aren't the scores exactly zero? Could it be the propogation of floating
     # point inaccuracies when summing?
-    assert np.allclose(metrics.generalized_dice(x, y), np.zeros(shape[0]), atol=1e-03)
+    assert_allclose(metrics.generalized_dice(x, y), np.zeros(shape[0]), atol=1e-03)
 
     x = np.ones((4, 32, 32, 32, 1), dtype=np.float64)
     y = x.copy()
@@ -73,34 +75,34 @@ def test_generalized_dice():
     # makes the generalized form slightly different from Dice.
     gd = metrics.generalized_dice(x, y, axis=(1, 2, 3)).numpy()
     dd = metrics.dice(x, y, axis=(1, 2, 3, 4)).numpy()
-    assert np.allclose(gd, dd, rtol=1e-02)  # is this close enough?
+    assert_allclose(gd, dd, rtol=1e-02)  # is this close enough?
 
 
 def test_jaccard():
     x = np.zeros(4)
     y = np.zeros(4)
     out = metrics.jaccard(x, y, axis=None).numpy()
-    assert np.allclose(out, 1)
+    assert_allclose(out, 1)
 
     x = np.ones(4)
     y = np.ones(4)
     out = metrics.jaccard(x, y, axis=None).numpy()
-    assert np.allclose(out, 1)
+    assert_allclose(out, 1)
 
     x = [0., 0., 1., 1.]
     y = [1., 1., 1., 1.]
     out = metrics.jaccard(x, y, axis=None).numpy()
     ref = 1. - scipy.spatial.distance.jaccard(x, y)
-    assert np.allclose(out, ref)
+    assert_allclose(out, ref)
     dice_out = metrics.dice(x, y, axis=None).numpy()
-    assert np.allclose(out, dice_out / (2. - dice_out))
+    assert_allclose(out, dice_out / (2. - dice_out))
 
     x = [0., 0., 1., 1.]
     y = [1., 1., 0., 0.]
     out = metrics.jaccard(x, y, axis=None).numpy()
     ref = 1. - scipy.spatial.distance.jaccard(x, y)
-    assert np.allclose(out, ref, atol=1e-07)
-    assert np.allclose(out, 0, atol=1e-07)
+    assert_allclose(out, ref, atol=1e-07)
+    assert_allclose(out, 0, atol=1e-07)
 
     x = np.ones((4, 32, 32, 32, 1), dtype=np.float32)
     y = x.copy()
@@ -110,7 +112,7 @@ def test_jaccard():
     jaccards = np.empty(x.shape[0])
     for i in range(x.shape[0]):
         jaccards[i] = 1. - scipy.spatial.distance.jaccard(x[i].flatten(), y[i].flatten())
-    assert np.allclose(metrics.jaccard(x, y, axis=(1, 2, 3, 4)), jaccards)
+    assert_allclose(metrics.jaccard(x, y, axis=(1, 2, 3, 4)), jaccards)
 
 
 def test_tversky():
@@ -121,12 +123,12 @@ def test_tversky():
     # Test that tversky and dice are same when alpha = beta = 0.5
     dice = metrics.dice(y_true, y_pred).numpy()
     tversky = metrics.tversky(y_true, y_pred, axis=(1, 2, 3), alpha=0.5, beta=0.5).numpy()
-    assert np.allclose(dice, tversky)
+    assert_allclose(dice, tversky)
 
     # Test that tversky and jaccard are same when alpha = beta = 1.0
     jaccard = metrics.jaccard(y_true, y_pred).numpy()
     tversky = metrics.tversky(y_true, y_pred, axis=(1, 2, 3), alpha=1., beta=1.).numpy()
-    assert np.allclose(jaccard, tversky)
+    assert_allclose(jaccard, tversky)
 
     with pytest.raises(ValueError):
         metrics.tversky([0., 0., 1.], [1., 0., 1.], axis=0)
