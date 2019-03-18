0c0ebc96a60b42cc333b838166bb8683e5de312b
Jakub Kaczmarzyk <jakubk@mit.edu>
2019-03-18 16:37:32 -0400

    import nobrainer.predictions and del tf

--- nobrainer/__init__.py
+++ nobrainer/__init__.py
@@ -9,7 +9,7 @@ if LooseVersion(tf.__version__) < LooseVersion("1.13.1"):
     raise ValueError(
         "tensorflow>=1.13.1 must be installed but found version {}"
         .format(tf.__version__))
-del LooseVersion
+del LooseVersion, tf
 
 from nobrainer._version import get_versions
 __version__ = get_versions()['version']
@@ -20,6 +20,7 @@ import nobrainer.layers
 import nobrainer.losses
 import nobrainer.metrics
 import nobrainer.models
+import nobrainer.prediction
 import nobrainer.training
 import nobrainer.transform
 import nobrainer.utils
