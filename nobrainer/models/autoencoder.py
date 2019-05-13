7de70e333ab4cd440384f17cfdb241208abe64db
wazeer zulfikar <wazeer.zulfikar@gmail.com>
2019-05-13 11:30:22 -0400

    Updated autoencoder with the required changes

--- nobrainer/models/autoencoder.py
+++ nobrainer/models/autoencoder.py
@@ -1,56 +1,80 @@
-"""Model definition for 3D Autoencoder.
+"""Model definition for Autoencoder.
 """
 
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow.keras import layers, models
 import math
 
 def autoencoder(input_shape, encoding_dim=512, n_base_filters=16, batchnorm=True, batch_size=None, name='autoencoder'):
-	"""Instantiate 3D Autoencoder architecture."""
+	"""Instantiate Autoencoder Architecture.
+
+    Parameters
+    ----------
+    input_shape: list or tuple of four ints, the shape of the input data. Omit
+        the batch dimension, and include the number of channels. Currently, only squares and cubes supported.
+    encoding_dim: int, the dimensions of the encoding of the input data. This would translate
+    	to a latent code of dimensions encoding_dimx1.
+    n_base_filters: int, number of base filters the models first convolutional layer. The subsequent layers
+    	have n_filters which are multiples of n_base_filters.
+    batchnorm: bool, whether to use batch normalization in the network.
+    batch_size: int, number of samples in each batch. This must be set when
+        training on TPUs.
+    name: str, name to give to the resulting model object.
+
+    Returns
+    -------
+    Model object.
+    Encoding layer.
+    """
 
 	conv_kwds = {
-		'kernel_size': (4, 4, 4),
+		'kernel_size': 4,
 		'activation': None,
 		'padding': 'same',
-		# 'kernel_regularizer': tf.keras.regularizers.l2(0.001),
+		'strides': 2
 	}
 
 	conv_transpose_kwds = {
-		'kernel_size': (4, 4, 4),
+		'kernel_size': 4,
 		'strides': 2,
 		'activation': None,
 		'padding': 'same',
-		# 'kernel_regularizer': tf.keras.regularizers.l2(0.001),
 	}
 
-	n_layers = int(math.log(input_shape[0], 2))
+	dimensions = input_shape[:-1]
+	if not dimensions[1:]==dimensions[:-1]:
+		raise ValueError('Dimensions should be of square or cube!')
+
+	Conv = getattr(layers, 'Conv{}D'.format(len(dimensions)))
+	ConvTranspose = getattr(layers, 'Conv{}DTranspose'.format(len(dimensions)))
+	n_layers = int(math.log(dimensions[0], 2))
 
 	# Input layer
-	inputs = x = layers.Input(shape=input_shape, batch_size=batch_size)
+	inputs = x = layers.Input(shape=input_shape, batch_size=batch_size, name='inputs')
 
 	# Encoder
 	for i in range(n_layers):
 		n_filters = min(n_base_filters*(2**(i)), encoding_dim)
 
-		x = layers.Conv3D(n_filters, strides=2, **conv_kwds)(x)
+		x = Conv(n_filters, **conv_kwds)(x)
 		if batchnorm:
 			x = layers.BatchNormalization()(x)
 		x = layers.ReLU()(x)
 
-	x = layers.Flatten()(x)
-	encoding = x = layers.Dense(encoding_dim, activation='tanh')(x)
+	# Encoding of the input image
+	encoding = x = layers.Flatten(name='Encoding')(x)
 	
 	# Decoder
 	x = layers.Reshape((1, 1, 1, encoding_dim))(x)
-	for i in range(n_layers):
-		n_filters = min(n_base_filters*(2**(n_layers-i-1)), encoding_dim)
+	for i in range(n_layers-1,0,-1):
+		n_filters = min(n_base_filters*(2**(i)), encoding_dim)
 
-		x = layers.Conv3DTranspose(n_filters, **conv_transpose_kwds)(x)
+		x = ConvTranspose(n_filters, **conv_transpose_kwds)(x)
 		if batchnorm:
 			x = layers.BatchNormalization()(x)
 		x = layers.LeakyReLU()(x)
 
 	# Output layer
-	outputs = tf.keras.layers.Conv3D(1, 3, activation='tanh', padding='same')(x)
+	outputs = Conv(1, 3, activation='sigmoid', padding='same')(x)
 
-	return tf.keras.models.Model(inputs=inputs, outputs=outputs)
+	return models.Model(inputs=inputs, outputs=outputs), encoding
