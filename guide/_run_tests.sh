a40d58b648e248721db5a8320c147381e300e714
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2020-01-10 23:54:27 -0500

    minor fixes

--- guide/_run_tests.sh
+++ guide/_run_tests.sh
@@ -10,10 +10,10 @@ jupyter-nbconvert --to python *.ipynb
 # Remove anything from `model.fit` and below in scripts.
 # Model fitting may crash travis.
 # Also remove references to `pip install`.
-sed -i -e '/model.fit/,$d; /!pip install/d;' *.py
+sed -i -e '/model.fit/,$d; /pip install/d;' *.py
 
 # Add a print statement to the bottom of each script so we know it finished.
 for f in *.py; do echo print\(\"++ FINISHED $f ++\"\) >> $f; done
 
 # Run!
-for f in *.py; do jupyter-run $f || break; done || exit 1
+for f in *.py; do jupyter-run $f || exit 1; done
