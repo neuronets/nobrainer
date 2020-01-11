58093fb7f4d3af61094d0149f7540a046cc3dcf4
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2020-01-10 23:45:41 -0500

    run notebook tests from script

---
+++ guide/_run_tests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Test the notebooks in this directory.
+
+set -e
+
+# Convert notebooks to .py files.
+jupyter-nbconvert --to python *.ipynb
+
+# Remove anything from `model.fit` and below in scripts.
+# Model fitting may crash travis.
+# Also remove references to `pip install`.
+sed -i -e '/model.fit/,$d; /!pip install/d;' *.py
+
+# Add a print statement to the bottom of each script so we know it finished.
+for f in *.py; do echo print\(\"++ FINISHED $f ++\"\) >> $f; done
+
+# Run!
+for f in *.py; do jupyter-run $f || break; done || exit 1
