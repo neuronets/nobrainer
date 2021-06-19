ff96c141ab2c154f0da3ea8e62f86a30cf9a3c68
Aakanksha Rana <40461936+Aakanksha-Rana@users.noreply.github.com>
2021-06-19 11:16:17 -0400

    Update vnet.py

--- nobrainer/models/vnet.py
+++ nobrainer/models/vnet.py
@@ -39,59 +39,52 @@ def up_stage(inputs, skip, filters, kernel_size=3, activation="relu", padding="S
     return convu
 
 
-def end_stage(inputs, kernel_size=3, activation="relu", padding="SAME"):
-    conv = Conv3D(1, kernel_size, activation=activation, padding="SAME")(inputs)
-    conv = Conv3D(1, 1, activation="sigmoid")(conv)
+def end_stage(inputs, n_classes = 1, kernel_size=3, activation="relu", padding="SAME"):
+    conv = Conv3D(filters= n_classes, kernel_size= kernel_size, activation=activation, padding="SAME")(inputs)
+    if n_classes == 1: 
+        conv = Conv3D(n_classes, 1, activation="sigmoid")(conv)
+    else:
+        conv = Conv3D(n_classes, 1, activation="softmax")(conv)
 
     return conv
 
 
-def vnet(
-    input_shape=(256, 256, 256, 1), kernel_size=3, activation="relu", padding="SAME"
-):
+def vnet(n_classes = 1, input_shape=(256, 256, 256, 1), kernel_size=3,
+                 activation="relu", padding="SAME", **kwargs):
     inputs = Input(input_shape)
 
-    conv1, pool1 = down_stage(
-        inputs, 16, kernel_size=kernel_size, activation=activation, padding=padding
-    )
-    conv2, pool2 = down_stage(
-        pool1, 32, kernel_size=kernel_size, activation=activation, padding=padding
-    )
-    conv3, pool3 = down_stage(
-        pool2, 64, kernel_size=kernel_size, activation=activation, padding=padding
-    )
-    conv4, _ = down_stage(
-        pool3, 128, kernel_size=kernel_size, activation=activation, padding=padding
-    )
+    conv1, pool1 = down_stage(inputs, 16,
+                              kernel_size=kernel_size,
+                              activation=activation,
+                              padding=padding)
+    conv2, pool2 = down_stage(pool1, 32,
+                              kernel_size=kernel_size,
+                              activation=activation,
+                              padding=padding)
+    conv3, pool3 = down_stage(pool2, 64,
+                              kernel_size=kernel_size,
+                              activation=activation,
+                              padding=padding)
+    conv4, _ = down_stage(pool3, 128,
+                          kernel_size=kernel_size,
+                          activation=activation,
+                          padding=padding)
     conv4 = SpatialDropout3D(0.5)(conv4, training=True)
 
-    conv5 = up_stage(
-        conv4,
-        conv3,
-        64,
-        kernel_size=kernel_size,
-        activation=activation,
-        padding=padding,
-    )
-    conv6 = up_stage(
-        conv5,
-        conv2,
-        32,
-        kernel_size=kernel_size,
-        activation=activation,
-        padding=padding,
-    )
-    conv7 = up_stage(
-        conv6,
-        conv1,
-        16,
-        kernel_size=kernel_size,
-        activation=activation,
-        padding=padding,
-    )
+    conv5 = up_stage(conv4, conv3, 64,
+                     kernel_size=kernel_size,
+                     activation=activation,
+                     padding=padding)
+    conv6 = up_stage(conv5, conv2, 32,
+                     kernel_size=kernel_size,
+                     activation=activation,
+                     padding=padding)
+    conv7 = up_stage(conv6, conv1, 16,
+                     kernel_size=kernel_size,
+                     activation=activation,
+                     padding=padding)
 
-    conv8 = end_stage(
-        conv7, kernel_size=kernel_size, activation=activation, padding=padding
-    )
+    conv8 = end_stage(conv7, n_classes = n_classes, kernel_size=kernel_size,
+                      activation=activation, padding=padding)
 
     return Model(inputs=inputs, outputs=conv8)
