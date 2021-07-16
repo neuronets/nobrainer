84c1d7269bba1f5fed65f0a3c57751b2c3dd296b
Aakanksha Rana <40461936+Aakanksha-Rana@users.noreply.github.com>
2021-07-16 18:10:22 -0400

    Update metrics.py

--- nobrainer/metrics.py
+++ nobrainer/metrics.py
@@ -128,5 +128,5 @@ def dice_coef_multilabel(y_true, y_pred):
     n_classes = tf.shape(y_pred)[-1]
     dice_coeff = 0
     for index in range(n_classes):
-        dice_coeff -= dice(y_true[:, :, :, :, index], y_pred[:, :, :, :, index])
+        dice_coeff -= dice(y_true[..., index], y_pred[..., index])
     return dice_coeff
