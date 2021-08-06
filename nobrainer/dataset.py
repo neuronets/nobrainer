2d5f931f4a47249416d34fb66f2f5bb792479537
dhritimandas <dhritimandas.25@gmail.com>
2021-08-05 21:22:35 -0400

    Update dataset.py

--- nobrainer/dataset.py
+++ nobrainer/dataset.py
@@ -144,7 +144,7 @@ def get_dataset(
             dataset = dataset.map(
                 lambda x, y: tf.cond(
                     tf.random.uniform((1,)) > 0.5,
-                    true_fn=lambda: addGaussianNoise(x, y),
+                    true_fn=lambda: centercrop(x, y),
                     false_fn=lambda: (x, y),
                 ),
                 num_parallel_calls=num_parallel_calls,
