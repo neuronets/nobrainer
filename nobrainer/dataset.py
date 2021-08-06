c10f928eca66a3fa39f8f52ec082447f4458d2c5
dhritimandas <dhritimandas.25@gmail.com>
2021-08-05 22:27:46 -0400

    Update dataset.py

--- nobrainer/dataset.py
+++ nobrainer/dataset.py
@@ -142,11 +142,12 @@ def get_dataset(
     if augment:
         if not scalar_label:
             dataset = dataset.map(
-                lambda x, y: tf.cond(
-                    tf.random.uniform((1,)) > 0.9,
-                    true_fn=lambda: apply_random_transform(x, y),
-                    false_fn=lambda: (x, y),
-                ),
+#                 lambda x, y: tf.cond(
+#                     tf.random.uniform((1,)) > 0.9,
+#                     true_fn=lambda: apply_random_transform(x, y),
+#                     false_fn=lambda: (x, y),
+#                 ),
+                  lambda x, y: addGaussianNoise(x, y),
                 num_parallel_calls=num_parallel_calls,
             )
         else:
