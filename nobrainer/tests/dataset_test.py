0364b19fb963a39a614d2646214fa3adb2e4f0ed
Adam Richie-Halford <richiehalford@gmail.com>
2021-10-28 11:09:53 -0700

    Add dataset.py tests to confirm that get_dataset can load 3D and 4D tensors

--- nobrainer/tests/dataset_test.py
+++ nobrainer/tests/dataset_test.py
@@ -1,7 +1,9 @@
+import glob
 import os.path as op
 import shutil
 import tempfile
 
+import nibabel as nib
 import numpy as np
 from numpy.testing import assert_array_equal
 import pytest
@@ -53,6 +55,58 @@ def test_get_dataset_maintains_order(
         np.concatenate([y for _, y in dset.as_numpy_iterator()]).flatten().astype(int)
     )
     assert_array_equal(y_orig, y_from_dset)
+    shutil.rmtree(temp_dir)
+
+
+def create_dummy_niftis(shape, n_volumes, outdir):
+    array_data = np.zeros(shape, dtype=np.int16)
+    affine = np.diag([1, 2, 3, 1])
+    array_img = nib.Nifti1Image(array_data, affine)
+    for volume in range(n_volumes):
+        nib.save(array_img, op.join(outdir, f"image_{volume}.nii.gz"))
+
+    return glob.glob(op.join(outdir, "image_*.nii.gz"))
+
+
+def test_get_dataset_errors():
+    temp_dir = tempfile.mkdtemp()
+    file_pattern = op.join(temp_dir, "does_not_exist-*.tfrec")
+    with pytest.raises(ValueError):
+        dataset.get_dataset(
+            file_pattern=file_pattern,
+            n_classes=1,
+            batch_size=1,
+            volume_shape=(256, 256, 256),
+        )
+
+
+@pytest.mark.parametrize("batch_size", [1, 2])
+@pytest.mark.parametrize("examples_per_shard", [1, 3])
+@pytest.mark.parametrize("volume_shape", [(64, 64, 64), (64, 64, 64, 3)])
+@pytest.mark.parametrize("num_parallel_calls", [1, 2])
+def test_get_dataset_shapes(
+    volume_shape, examples_per_shard, batch_size, num_parallel_calls
+):
+    temp_dir = tempfile.mkdtemp()
+    nifti_paths = create_dummy_niftis(volume_shape, 10, temp_dir)
+    filepaths = [(x, i) for i, x in enumerate(nifti_paths)]
+    file_pattern = write_tfrecs(
+        filepaths, temp_dir, examples_per_shard=examples_per_shard
+    )
+    dset = dataset.get_dataset(
+        file_pattern=file_pattern,
+        n_classes=1,
+        batch_size=batch_size,
+        volume_shape=volume_shape,
+        scalar_label=True,
+        n_epochs=1,
+        num_parallel_calls=num_parallel_calls,
+    )
+
+    output_volume_shape = volume_shape if len(volume_shape) > 3 else volume_shape + (1,)
+    output_volume_shape = (batch_size,) + output_volume_shape
+    shapes = [x.shape for x, _ in dset.as_numpy_iterator()]
+    assert all([_shape == output_volume_shape for _shape in shapes])
     shutil.rmtree(temp_dir)
 
 
