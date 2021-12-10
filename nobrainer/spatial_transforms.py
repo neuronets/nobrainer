43d4cc748d4734b1581ce9efbc45cbb1ec067cfa
Aakanksha Rana <40461936+Aakanksha-Rana@users.noreply.github.com>
2021-12-10 11:43:15 -0500

    Update nobrainer/spatial_transforms.py
    Co-authored-by: Satrajit Ghosh <satrajit.ghosh@gmail.com>

--- nobrainer/spatial_transforms.py
+++ nobrainer/spatial_transforms.py
@@ -129,7 +129,7 @@ def resize(x, y=None, trans_xy=False, size=[32, 32], mode="bicubic"):
 def randomflip_leftright(x, y=None, trans_xy=False):
     """
     Randomly flips the input and label randomly with a given probability.
-    x and y is torch Tensor or numpy array,
+    x and y are Tensors or numpy arrays,
     it is expected to be of rank 3
 
     Args:
