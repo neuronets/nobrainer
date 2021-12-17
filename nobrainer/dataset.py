fc85735907b10d66ed8615f91a497209f464967b
Aakanksha Rana <40461936+Aakanksha-Rana@users.noreply.github.com>
2021-12-16 21:11:20 -0500

    Update dataset.py

--- nobrainer/dataset.py
+++ nobrainer/dataset.py
@@ -9,8 +9,6 @@ import tensorflow as tf
 from .io import _is_gzipped
 from .tfrecord import parse_example_fn
 from .volume import (
-    apply_random_transform,
-    apply_random_transform_scalar_labels,
     binarize,
     replace,
     standardize,
