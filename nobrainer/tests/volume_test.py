74bb1ade27f31281bb20caab3bf28967b8178988
Aakanksha Rana <40461936+Aakanksha-Rana@users.noreply.github.com>
2022-01-13 11:22:36 -0500

    Update volume_test.py

--- nobrainer/tests/volume_test.py
+++ nobrainer/tests/volume_test.py
@@ -86,17 +86,6 @@ def test_apply_random_transform(shape, scalar_labels):
     assert np.any(x0 % 1)
     assert np.any(x1 % 1)
 
-    # Depreacation warning test
-    with warnings.catch_warnings(record=True) as w:
-        warnings.simplefilter("always")
-        x = np.ones(shape).astype(np.float32)
-        transform_func(x, y_in)
-
-        assert len(w) == 1
-        assert issubclass(w[-1].category, PendingDeprecationWarning)
-        assert "moved" in str(w[-1].message)
-
-
 def test_binarize():
     x = [
         0.49671415,
