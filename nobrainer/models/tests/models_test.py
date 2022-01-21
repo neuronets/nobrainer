3abf298d308987b6c84acd980008a299fe88a67b
Aakanksha Rana <40461936+Aakanksha-Rana@users.noreply.github.com>
2022-01-21 13:46:45 -0500

    Update models_test.py

--- nobrainer/models/tests/models_test.py
+++ nobrainer/models/tests/models_test.py
@@ -14,7 +14,7 @@ from ..meshnet import meshnet
 from ..progressivegan import progressivegan
 from ..unet import unet
 from ..vnet import vnet
-from ..vox2vox import Vox_discriminator, vox_gan
+from ..vox2vox import Vox_ensembler, vox_gan
 
 
 def model_test(model_cls, n_classes, input_shape, kwds={}):
