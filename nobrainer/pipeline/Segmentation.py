9ab2bbcc2d03277035b2de63517208d84cb09023
Aakanksha Rana <40461936+Aakanksha-Rana@users.noreply.github.com>
2022-01-26 14:36:28 -0500

    Create Segmentation.py

---
+++ nobrainer/pipeline/Segmentation.py
@@ -0,0 +1,23 @@
+
+
+import numpy as np
+
+class base_class():
+  
+
+class Segmentation(base_class):
+  
+  def fit(): 
+    #estimate or train a model
+  def fit_transform():
+    #train and apply estimates to input
+  def load():
+    #load a saved model
+  def predict():
+    #apply trained model to input
+  def save():
+    
+  def transform():
+    #transform inputs using estimates
+  def evaluate():
+    # evaluate using a trained model, test inputs and labels.
