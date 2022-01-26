c89d80218fb6c17a7abd66c241ca1b9f353afdf5
Aakanksha Rana <40461936+Aakanksha-Rana@users.noreply.github.com>
2022-01-26 15:01:22 -0500

    Update Segmentation.py

--- nobrainer/pipeline/Segmentation.py
+++ nobrainer/pipeline/Segmentation.py
@@ -11,13 +11,12 @@ class Segmentation(base_class):
     #estimate or train a model
   def fit_transform():
     #train and apply estimates to input
+  def transform():
+    #transform inputs using estimates
   def load():
     #load a saved model
   def predict():
     #apply trained model to input
   def save():
-    
-  def transform():
-    #transform inputs using estimates
   def evaluate():
     # evaluate using a trained model, test inputs and labels.
