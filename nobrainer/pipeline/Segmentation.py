74d59b3fa2271dab788fe4339dd389fd3136e63d
Aakanksha Rana <40461936+Aakanksha-Rana@users.noreply.github.com>
2022-01-26 15:45:57 -0500

    Update Segmentation.py

--- nobrainer/pipeline/Segmentation.py
+++ nobrainer/pipeline/Segmentation.py
@@ -6,9 +6,34 @@ class base_class():
   
 
 class Segmentation(base_class):
+  """
+  Segmentation API. 
   
-  def fit(): 
+  Sequentially applies transforms, train, predict 
+  and evaluate the segmentation model. 
+  
+  """
+  def __init__(
+    self, 
+    n_classes,
+    input_shape,
+    activation="relu",
+    batchnorm=False,
+    batch_size=None,
+    model_name='unet', 
+  ):
+    self.n_classes = n_classes, 
+    self.input_shape = input_shape 
+    self.activation = activation
+    self.batchnorm= batchnorm
+    self.model_name= model_name
+  
+  def fit(self, x, y, **kwds): 
     #estimate or train a model
+    
+    
+    
+    
   def fit_transform():
     #train and apply estimates to input
   def transform():
