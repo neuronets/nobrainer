cd3c86f7acd4c957f4333909f47c8fca4cf4d283
Hoda Rajaei <rajaei.hoda@gmail.com>
2022-03-10 10:29:42 -0800

    update generation api

--- nobrainer/processing/generation.py
+++ nobrainer/processing/generation.py
@@ -7,44 +7,62 @@ import tensorflow as tf
 from .base import BaseEstimator
 from .. import losses
 from ..dataset import get_dataset
+from ..models.progressivegan import progressivegan
 from ..training import ProgressiveGANTrainer
 
 
 class ProgressiveGeneration(BaseEstimator):
     """Perform generation type operations"""
 
-    # state_variables = ["block_shape_", "volume_shape_", "scalar_labels_"]
     state_variables = ["resolution_batch_size_map_"]
 
-    def __init__(self, base_model, model_args=None):
-        if not isinstance(base_model, str):
-            self.base_model = base_model.__name__
-        else:
-            self.base_model = base_model
-        self.model_ = None
-        self.model_args = model_args or {}
-        self.resolution_batch_size_map_ = None
-        self.reolutions_ = sorted(list(self.resolution_batch_size_map_.keys()))
-        self.file_pattern = "*res-%03d*.tfrec"
+    def __init__(self,
+                 latent_size=256,
+                 label_size=0,
+                 num_channels=1,
+                 dimensionality=3,
+                 g_fmap_base=1024,
+                 d_fmap_base=1024,
+                 #g_optimizer=None,
+                 #g_opt_args=None,
+                 #d_optimizer=None,
+                 #d_opt_args=None,
+                 #g_loss=losses.wasserstein,
+                 #d_loss=losses.wasserstein,
+                 #resolution_batch_size_map= None,
+                 #input_file_pattern="*res-%03d*.tfrec"
+    ):
+        self.model_ = progressivegan
+        self.latent_size = latent_size
+        self.label_size = label_size
+        self.g_fmap_base = g_fmap_base
+        self.d_fmap_base = d_fmap_base
+        self.num_channels = num_channels
+        self.dimensionality = dimensionality
+        
+        #self.g_optimizer = g_optimizer
+        #self.g_opt_args = g_opt_args or {}
+        #self.d_optimizer = d_optimizer
+        #self.d_opt_args = d_opt_args or {}
+        #self.g_loss = g_loss
+        #self.d_loss = d_loss
+        #self.file_pattern = input_file_pattern
+        #self.resolution_batch_size_map_ = resolution_batch_size_map or {}
+        #self.reolutions_ = sorted(list(self.resolution_batch_size_map_.keys()))       
 
     def fit(
         self,
-        dataset_train,
-        dataset_validate=None,
         epochs=2,
         checkpoint_dir=os.getcwd(),
-        latent_size=256,
-        g_fmap_base=1024,
-        d_fmap_base=1024,
-        num_channels=1,
-        dimensionality=3,
         # TODO: figure out whether optimizer args should be flattened
-        g_optimizer=None,
-        g_opt_args=None,
-        d_optimizer=None,
-        d_opt_args=None,
-        g_loss=losses.wasserstein,
-        d_loss=losses.wasserstein,
+        g_optimizer = None,
+        g_opt_args = None,
+        d_optimizer = None,
+        d_opt_args = None,
+        g_loss = losses.wasserstein,
+        d_loss = losses.wasserstein,
+        resolution_batch_size_map= None,
+        input_file_pattern="*res-%03d*.tfrec",
         warm_start=False,
         multi_gpu=False,
         num_parallel_calls=None,
@@ -53,6 +71,8 @@ class ProgressiveGeneration(BaseEstimator):
         # TODO: check validity of datasets
 
         n_classes = 1  # dummy labels as this is unsupervised training
+        self.resolution_batch_size_map_ = resolution_batch_size_map or {}
+        self.reolutions_ = sorted(list(self.resolution_batch_size_map_.keys()))
 
         # create checkpoint sub-dirs
         checkpoint_dir = Path(checkpoint_dir)
@@ -61,7 +81,6 @@ class ProgressiveGeneration(BaseEstimator):
         model_dir = checkpoint_dir / "saved_models"
         log_dir = checkpoint_dir / "logs"
 
-        checkpoint_dir.mkdir(exist_ok=True)
         generated_dir.mkdir(exist_ok=True)
         model_dir.mkdir(exist_ok=True)
         log_dir.mkdir(exist_ok=True)
@@ -85,13 +104,12 @@ class ProgressiveGeneration(BaseEstimator):
             d_opt_args_tmp.update(**d_opt_args)
             d_opt_args = d_opt_args_tmp
 
-        def _create(base_model):
+        def _create():
             # Instantiate the generator and discriminator
-            self.generator_, self.discriminator_ = base_model(
-                latent_size=latent_size,
-                g_fmap_base=g_fmap_base,
-                d_fmap_base=d_fmap_base,
-                **self.model_args
+            self.generator_, self.discriminator_ = self.model_(
+                latent_size=self.latent_size,
+                g_fmap_base=self.g_fmap_base,
+                d_fmap_base=self.d_fmap_base,
             )
 
         def _compile():
@@ -103,16 +121,16 @@ class ProgressiveGeneration(BaseEstimator):
             )
 
             self.pgan_trainer.compile(
-                g_optimizer=g_optimizer(**g_opt_args),
-                d_optimizer=d_optimizer(**d_opt_args),
-                g_loss_fn=g_loss,
-                d_loss_fn=d_loss,
+                g_optimizer=self.g_optimizer(**self.g_opt_args),
+                d_optimizer=self.d_optimizer(**self.d_opt_args),
+                g_loss_fn=self.g_loss,
+                d_loss_fn=self.d_loss,
             )
 
         # without warmstart and multi gpu for now
-        mod = importlib.import_module("..models", "nobrainer.processing")
-        base_model = getattr(mod, self.base_model)
-        _create(base_model)
+        #mod = importlib.import_module("..models", "nobrainer.processing")
+        #base_model = getattr(mod, self.base_model)
+        _create()
         print(self.generator_.summary())
         print(self.discriminator_.summary())
 
@@ -177,7 +195,6 @@ class ProgressiveGeneration(BaseEstimator):
     def generate(
         self,
         x,
-        latent_size=1024,
     ):
         """generate a synthetic image using the trained model"""
         if self.model_ is None:
@@ -185,9 +202,9 @@ class ProgressiveGeneration(BaseEstimator):
         import nibabel as nib
         import numpy as np
 
-        latents = tf.random.normal((1, latent_size))
+        latents = tf.random.normal((1, self.latent_size))
         generate = self.generator_.signatures["serving_default"]
         img = generate(latents)["generated"]
         img = np.squeeze(img)
         img = nib.Nifti1Image(img.astype(np.uint16), np.eye(4))
-        return img
+        return img,latents
